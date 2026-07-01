7f3cfbf66f64bc8f07da74280ba195d8b9ef77af
SailorJette <belluspes2@gmail.com>
2026-07-01 11:04:04 +0200

    finished helloworld tutorial

--- tutorials/hello-world.cs
+++ tutorials/hello-world.cs
@@ -1,4 +1,3 @@
-using System;
 
 namespace HelloWorld
 {
@@ -6,7 +5,30 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Challü World!");
+            string ühüman = "Ümann";
+            string obst = "Apfel";
+            //Console.WriteLine("Challü World!");
+            ühüman = "Gelühbtester Ümann";
+            //Console.WriteLine("Wen lühbe ich am meisten?:" + ühüman);
+            // Console.WriteLine($"Meinen {ühüman} lühbe ich am meisten!");
+            //Console.WriteLine($"Das Obst {obst} hat {obst.Length} Buchstaben.");
+
+            string greeting = "      Hello World!       ";
+            string trimmedGreeting = greeting.Trim();
+            //Console.WriteLine($"[{trimmedGreeting}]");
+            string sayHello = "Moin Ümannü";
+            Console.WriteLine(sayHello);
+            sayHello = sayHello.Replace("Moin", "Challü");
+            Console.WriteLine(sayHello);
+            Console.WriteLine(sayHello.ToUpper());
+            Console.WriteLine(sayHello.ToLower());
+
+            string songLyrics = "You say goodbye, and I say helloo";
+            Console.WriteLine(songLyrics.EndsWith("hello") || songLyrics.StartsWith("You"));
+            Console.WriteLine(songLyrics.Contains("greetings"));
+
         }
     }
 }
+
+// https://learn.microsoft.com/de-de/dotnet/csharp/tour-of-csharp/tutorials/hello-world
